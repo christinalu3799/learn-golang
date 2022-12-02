938f94316bd4bff32aeaf87a05175dc2cfda2f02
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-02 15:39:18 -0500

    converting variable types

--- src/github.com/christinalu3799/firstapp/main.go
+++ src/github.com/christinalu3799/firstapp/main.go
@@ -4,8 +4,27 @@
 package main
 
 // fmt is a popular package that contains functions for formatting text, including printing to the console
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
+// can group variables like this to show they're all related at the package level
+var (
+	actorName    string = "Elisabeth Sladen"
+	companian    string = "Sarah Jane Smith"
+	doctorNumber int    = 3
+	season       int    = 11
+)
+
+// ============================ TERMS & DEFINITIONS =============================
+// shadowing: variables defined in the inner most scope takes precedence
+// variables HAVE to be used if they are defined. otherwise, you will get an error
+// naming convention: lowercase variables are scoped with the package
+// --- uppercase variables are globally visible
+// --- block scope: defined within a function
+// --- keep acronyms as all UPPERCASE
+// ==============================================================================
 // implementing a main function to print a message to the console. a "main" function executes by default when you run the main package
 func main() {
 	// 3 ways to declare variables
@@ -19,5 +38,18 @@ func main() {
 	k := 46
 	fmt.Println(i, j, k)
 	// string formatter - %v gives us value and %T gives us type
-	fmt.Printf("%v, %T", j, j)
+	fmt.Printf("%v, %T\n", j, j)
+	// ======================= CONVERTING VARIABLE TYPES =========================
+	var l float32
+	l = float32(i)
+	// before
+	fmt.Printf("%v, %T\n", i, i)
+	// after
+	fmt.Printf("%v, %T\n", l, l)
+	// ======================= CONVERTING INT TO ASCII ===========================
+	var m string
+	// need to import strconv package to properly do this!
+	m = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", m, m)
+
 }
