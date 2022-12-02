37e111e22854bf5852dd1e95a2198a45940d7ce0
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-02 15:27:09 -0500

    declaring variables 3 different ways

--- src/github.com/christinalu3799/firstapp/main.go
+++ src/github.com/christinalu3799/firstapp/main.go
@@ -8,5 +8,16 @@ import "fmt"
 
 // implementing a main function to print a message to the console. a "main" function executes by default when you run the main package
 func main() {
-	fmt.Println("Hello Go!")
+	// 3 ways to declare variables
+	// 1. Used to declare a variable when you don't want to initialize it yet
+	var i int
+	i = 42
+	// 2. all in 1 line, useful when go doesn't have enough info to assign the type you want
+	var j int = 50
+	// 3. allow go to figure out data type
+	// infers whole numbers are type int. if you add a decimal, it will recognize the variable as type float64.
+	k := 46
+	fmt.Println(i, j, k)
+	// string formatter - %v gives us value and %T gives us type
+	fmt.Printf("%v, %T", j, j)
 }
