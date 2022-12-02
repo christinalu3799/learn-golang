41c63becb05da7dc365928519168e90e50f49fc6
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-02 15:04:47 -0500

    installed go and printed message to the console

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+// Declare a main package (a way to group functions, the package is made up of all the files in the same directory)
+package main
+
+// fmt is a popular package that contains functions for formatting text, including printing to the console
+import "fmt"
+
+// implementing a main function to print a message to the console. a "main" function executes by default when you run the main package
+func main() {
+	fmt.Println("Hello world!")
+}
